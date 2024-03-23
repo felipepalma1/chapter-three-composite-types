7da1f8cf56164dd484cd93d83d52d4421d12601f
FPC <felipepalmacastro@gmail.com>
2024-03-23 17:19:39 -0300

    Initial commit

---
+++ common.go
@@ -0,0 +1,6 @@
+package chapter_three_composite_types
+
+func SimpleArray() [3]int {
+	var x [3]int
+	return x
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/felipepalma1/chapter-three-composite-types
+
+go 1.22
