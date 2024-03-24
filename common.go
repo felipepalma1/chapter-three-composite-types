0d0041be27a96f5e8e12074d4200904c9c4e3407
FPC <felipepalmacastro@gmail.com>
2024-03-23 21:05:55 -0300

    Testing Upstream Dependency.

--- common.go
+++ common.go
@@ -1,5 +1,7 @@
 package chapter_three_composite_types
 
+import "github.com/felipepalma1/chapter-three-special-array/operations"
+
 func SimpleArray() [3]int {
 	var x [3]int
 	return x
@@ -8,4 +10,8 @@ func SimpleArray() [3]int {
 func SimpleInitializedArray() [3]int {
 	var x = [3]int{10, 20, 30}
 	return x
+}
+
+func NewSpecialArray() [12]int {
+	return operations.NewSparseArray()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/felipepalma1/chapter-three-composite-types
 
 go 1.22
+
+require github.com/felipepalma1/chapter-three-special-array v0.0.0-20240323235717-49741e6029b7 // indirect
